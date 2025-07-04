e38095b79cd7eee79af9d7bb2d018c50faa94836
Jan Szejko <janek37@gmail.com>
2025-07-04 20:15:27 +0200

    Fix get_words_from_line

--- qwantz_metadata/utils.py
+++ qwantz_metadata/utils.py
@@ -8,7 +8,7 @@ def get_words_from_line(line: str) -> list[str]:
     line_text = re.sub(r"〚[^〛]*〛", "", line_text)
     line_text = re.sub(r"\w*⦃[^⦄]*⦄\w*", "", line_text)
     line_text = re.sub(r"\w*…\w*", "", line_text)
-    return re.findall(r"\w+", line_text) + re.findall(r"\w+(-\w+)+", line_text)
+    return re.findall(r"\w+", line_text) + re.findall(r"\w+(?:-\w+)+", line_text)
 
 
 def strip_line(line: str) -> str:
